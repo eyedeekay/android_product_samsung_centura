44fd90b0d5170f2330dc752fc9275671e6d3e7c4
cmotc <w4nder1n9w12ard+cmotc@gmail.com>
2014-10-21 23:42:29 -0400

    changed ROM backup script a little

--- copy_current_to_product_mirror.sh
+++ copy_current_to_product_mirror.sh
@@ -1,8 +1,8 @@
 #! /bin/sh
-cp -v /home/devhost/Replicant/system/home/android/out/target/product/schS738c/boot.img /home/devhost/android/android_product_samsung_centura/ROM/boot.img
-cp -v /home/devhost/Replicant/system/home/android/out/target/product/schS738c/ramdisk.img /home/devhost/android/android_product_samsung_centura/ROM/ramdisk.img
-cp -v /home/devhost/Replicant/system/home/android/out/target/product/schS738c/ramdisk-recovery.img /home/devhost/android/android_product_samsung_centura/ROM/ramdisk-recovery.img
-cp -v /home/devhost/Replicant/system/home/android/out/target/product/schS738c/recovery.img /home/devhost/android/android_product_samsung_centura/ROM/recovery.img
-cp -v /home/devhost/Replicant/system/home/android/out/target/product/schS738c/system.img /home/devhost/android/ROMsystem.img
+cp -v $HOME/Replicant/system/home/android/out/target/product/schS738c/boot.img ./ROM/boot.img
+cp -v $HOME/Replicant/system/home/android/out/target/product/schS738c/ramdisk.img ./ROM/ramdisk.img
+cp -v $HOME/Replicant/system/home/android/out/target/product/schS738c/ramdisk-recovery.img ./ROM/ramdisk-recovery.img
+cp -v $HOME/Replicant/system/home/android/out/target/product/schS738c/recovery.img ./ROM/recovery.img
+cp -v $HOME/Replicant/system/home/android/out/target/product/schS738c/system.img ./ROMsystem.img
 
 
